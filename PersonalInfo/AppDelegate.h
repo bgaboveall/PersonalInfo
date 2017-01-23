c7389101bedb61a97718c1a58f9fec2081258190
Boyko Avramov <boykoavramov@MacBook-Pro-2.local>
2017-01-23 15:22:19 -0500

    Initial Commit

---
+++ PersonalInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonalInfo
+//
+//  Created by Boyko Avramov on 2017-01-23.
+//  Copyright © 2017 Boyko Avramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonalInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonalInfo
+//
+//  Created by Boyko Avramov on 2017-01-23.
+//  Copyright © 2017 Boyko Avramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
